eb4dfd8f274c7bc43a0948275013bca30a014c99
nolawit23 <eyerusalemnolawit@gmail.com>
2025-10-27 12:17:02 -0700

    Remove failing test for CI

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,9 +25,4 @@ RSpec.describe User, type: :model do
     user = build(:user, password: nil)
     expect(user).not_to be_valid
   end
-
-  # it "fails intentionally for CI demonstration" do
-  # This method does not exist, so it will fail
-  # expect(User.new.non_existent_method).to eq(nil)
-  # end
 end
